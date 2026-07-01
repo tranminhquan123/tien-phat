d6d15819dc2ee6bf8c463d282602a84e846436a3
tranminhquan123 <157604927+tranminhquan123@users.noreply.github.com>
2026-07-01 12:04:11 +0700

    feat: add team link to next layout

--- client/src/components/AdminLayoutNext.tsx
+++ client/src/components/AdminLayoutNext.tsx
@@ -1,5 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export function AdminLayoutNext() {
-  return <main><Outlet /></main>;
+  return (
+    <div className="flex h-screen">
+      <aside className="w-64 bg-dark-900 p-4 text-white">
+        <NavLink to="/admin/doi-ngu">Nhân viên</NavLink>
+      </aside>
+      <main className="flex-1 overflow-y-auto p-6"><Outlet /></main>
+    </div>
+  );
 }
